74ce1c7ea29b6d6bf7df1033d751182a4e46148f
RyanLin <rrr20020910@gmail.com>
2022-05-13 21:47:26 +0800

    feat: game object header file

---
+++ lib/card/item/item.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "../../cstd.h"
+
+//return false if card does not successfully used
+//Otherwise return true
+
+//Gun
+bool schofield_ability(void);
+bool volcanic_ability(void);
+bool remington_ability(void);
+bool rev_carabine_ability(void);
+bool winchester_ability(void);
+
+//Horse
+bool mustang_ability(void);
+bool appaloosa_ability(void);
+
+//Other
+bool jail_ability(void);
+bool dynamite_ability(void);
+bool barrel_ability(void);
+
+//Brown
+bool bang_ability(void);
+bool miss_ability(void);
+bool beer_ability(void);
+bool cat_balou_ability(void);
+bool panic_ability(void);
+bool duel_ability(void);
+bool general_store_ability(void);
+bool indians_ability(void);
+bool stagecoach_ability(void);
+bool gatling_ability(void);
+bool saloon_ability(void);
+bool wells_fargo_ability(void);
