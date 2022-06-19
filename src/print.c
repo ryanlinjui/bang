4fc674cc547d3c399280c8a5c8d5c1363c6a86b9
ryanlinjui <ryanlinjui@gmail.com>
2022-06-19 15:26:54 +0800

    modify: delete existed define var

--- src/print.c
+++ src/print.c
@@ -5,12 +5,6 @@
 #include "player.h"
 #include "card.h"
 
-#define MAX_HAND_PRINT 6
-
-#define CARD_WIDTH   17
-#define CARD_LENGTH  12
-#define CARD_IMG_FILEPATH "./assets/card-image.txt"
-
 // TODO: system msgm
 // somebody play card 
 // somebody die
